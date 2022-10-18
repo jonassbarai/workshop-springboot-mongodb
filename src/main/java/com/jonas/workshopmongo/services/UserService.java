f5ebd3fdd8ba7d26d14d8f9b5fe68be9fe55b6d6
jonassbarai <jonas_sbarai@hotmail.com>
2022-10-17 21:14:45 -0300

    adcionando usuario com método POST

--- src/main/java/com/jonas/workshopmongo/services/UserService.java
+++ src/main/java/com/jonas/workshopmongo/services/UserService.java
@@ -3,10 +3,13 @@ package com.jonas.workshopmongo.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.xml.crypto.dsig.XMLObject;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.jonas.workshopmongo.domain.User;
+import com.jonas.workshopmongo.dto.UserDTO;
 import com.jonas.workshopmongo.repository.UserRepository;
 import com.jonas.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -22,6 +25,15 @@ public class UserService {
 
 	public User findById(String id) {
 		return repo.findById(id).orElseThrow(() -> new ObjectNotFoundException("usuário não encontrado"));			
+		
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(),objDto.getName(),objDto.getEmail());
 		
 	}
 }
